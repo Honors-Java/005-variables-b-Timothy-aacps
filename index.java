49b1e7157225ecb8e779e4323c3165207b54b58a
Timothyaacps <timotheous077@gmail.com>
2020-10-19 22:10:07 +0000

    I added all of the randomness to the code and got rid of the comments of the original project for easier viewing

--- index.java
+++ index.java
@@ -1,25 +1,30 @@
+int xPosition = 250;
+int yPosition = 250;
+int circleWidth = 20;
+int circleLength = 20;
+
 void setup() {
 	size(500, 500);
 }
 
 void draw() {
-// Step 1: Write code that draws the following screenshots 
-// with hard-coded values. (Feel free to use colors 
-// instead of grayscale.)
 
-// Step 2: Replace all of the hard-coded numbers with 
-// variables.
+ int rando = random(-25, 25);
+ int rY = random(-25, 25);
+ int r = random(0, 255);
+ int g = random(0, 255);
+ int b = random(0, 255);
 
-// Step 3: Write assignment operations in draw() that 
-// change the value of the variables.
-// For example, “variable1 = variable1 + 2;”. 
-
-// Step 4: Make it so when the program runs the ball slides off the screen exiting at the point (500, 500)
-
-  background(255);
   stroke(0);
+  fill(r, g, b);
+  ellipse(mouseX + rando,mouseY + rY,circleLength,circleWidth);
 
-
-  
-
+}
+void mousePressed(){
+  int randoX = random(0,400);
+  int randoY = random(0,400);
+  int randoW = random(0,400);
+  int randoL = random(0,400);
+  background(255);
+  rect(randoX,randoY,randoW,randoL);
 }
